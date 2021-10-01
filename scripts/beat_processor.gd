8118af8ec1aa73b0e559cea468fbb7e2eb90cc0b
Arthur Aslanyan <arthur.e.aslanyan@gmail.com>
2021-10-01 12:34:48 +0600

    Testing different tempos

--- scripts/beat_processor.gd
+++ scripts/beat_processor.gd
@@ -1,14 +1,19 @@
 class_name BeatProcessor
 
-var _hit := false
-var _count := 0
+var apm := 60
+
+var _last_time := -1.0
 
 
 # Register hit
-func hit() -> bool:
-	_count += 1
-	return _hit and _count < 2
+func hit(time: float) -> bool:
+	var result := false
+	
+	# Check if index is integral, so it's aligned to the beat
+	var index := time / 60 * apm
+	if index == floor(index):
+		result = _last_time != time
+			
+	_last_time = time
+	return result
 
-
-func play():
-	_hit = true
--- tests/beat.gd
+++ tests/beat.gd
@@ -2,6 +2,7 @@ extends WAT.Test
 
 var bp: BeatProcessor
 
+
 func pre() -> void:
 	bp = BeatProcessor.new()
 
@@ -10,17 +11,22 @@ func title() -> String:
 	return "Beat Processor Test"
 	
 
-func test_register_hit():
-	asserts.is_false(bp.hit(), "Hit should be detected, but missed due to not playing")
-
-
 func test_register_miss_if_extra_hit():
-	bp.play()
-	var _hit := bp.hit()
-	asserts.is_false(bp.hit(), "Miss should be registered if extra hit done")
+	var _hit := bp.hit(1)
+	asserts.is_false(bp.hit(1), "Miss should be registered if extra hit done on the same time")
 
 
 func test_register_hit_on_beat():
-	bp.play()
-	asserts.is_true(bp.hit(), "Hit should be registered on beat")
+	asserts.is_true(bp.hit(0), "Hit should be registered on beat 0")
+	asserts.is_true(bp.hit(1), "Hit should be registered on beat 1")
+	asserts.is_true(bp.hit(2), "Hit should be registered on beat 2")
+	
 
+func test_register_miss_out_of_beat():
+	asserts.is_false(bp.hit(0.5), "Miss should be registered out of beat")
+	
+
+func test_different_tempo_hit_on_beat():
+	bp.apm = 80
+	asserts.is_true(bp.hit(0.75), "Hit should be registered on second beat (0.75)")
+	asserts.is_true(bp.hit(1.5), "Hit should be registered on third beat (1.5)")
