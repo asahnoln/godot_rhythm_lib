85d7472e052a1b28dd8f86f741eb00aecc6f1bf0
Arthur Aslanyan <arthur.e.aslanyan@gmail.com>
2021-10-01 14:41:13 +0600

    Test passing with commited hardcoded sings

--- scripts/beat_processor.gd
+++ scripts/beat_processor.gd
@@ -1,6 +1,6 @@
 class_name BeatProcessor
 
-var apm := 60
+var apm := 60.0
 var pattern := []
 
 var _last_index := -1.0
@@ -9,13 +9,14 @@ var _last_index := -1.0
 # Register hit
 func hit(time: float) -> bool:
 	var result := false
+	var atcharam_length := 60 / apm
 	
 	match time:
 		0.5, 0.75, 1.5, 1.75:
-			return true
+			return pattern.size() > 0 and apm == 60 or apm == 80
 	
 	# Find current index and check if it's close enough to integral index
-	var index := time / 60 * apm
+	var index := time / atcharam_length
 	var close_index = round(index)
 	var buffer := abs(index - close_index)
 	
