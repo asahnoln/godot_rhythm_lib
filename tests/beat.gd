949d4983ec66b5e21c363a8b7864d05a55e01947
Arthur Aslanyan <arthur.e.aslanyan@gmail.com>
2021-10-01 13:29:09 +0600

    Failing tests for next tasks
    - [CHANGED] Same hit - now within buffer
    - [ADDED] Test for given pattern

--- tests/beat.gd
+++ tests/beat.gd
@@ -11,9 +11,14 @@ func title() -> String:
 	return "Beat Processor Test"
 	
 
-func test_register_miss_if_extra_hit():
+func test_register_miss_if_extra_hit_within_buffer():
+	bp.apm = 120
+	
 	var _hit := bp.hit(1)
 	asserts.is_false(bp.hit(1), "Miss should be registered if extra hit done on the same time")
+	
+	_hit = bp.hit(9.477333)
+	asserts.is_false(bp.hit(9.5), "Miss should be registered if extra hit done on within same buffer")
 
 
 func test_register_hit_on_beat():
@@ -37,3 +42,9 @@ func test_buffer_to_hit():
 	asserts.is_true(bp.hit(9.477333), "Hit should be registered with enough buffer until real beat")
 	asserts.is_true(bp.hit(9.525333), "Hit should be registered with enough buffer after real beat")
 	asserts.is_true(bp.hit(47.557335), "Another buffer hit")
+	
+func test_beat_on_given_pattern():
+	bp.apm = 60
+	bp.pattern = [2, 1, 1]
+	
+	asserts.is_true(bp.hit(0.75), "Hit should be registered on third pattern sol")
