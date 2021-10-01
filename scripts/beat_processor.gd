36a867087c18f8c71059452117d8ea9b58470324
Arthur Aslanyan <arthur.e.aslanyan@gmail.com>
2021-10-01 17:31:26 +0600

    Commiting sins with patterns
    - [ADDED] Match patterns
    - [ADDED] Hardcode indexing

--- scripts/beat_processor.gd
+++ scripts/beat_processor.gd
@@ -1,7 +1,7 @@
 class_name BeatProcessor
 
 var apm := 60.0
-var pattern := []
+var pattern := [4]
 
 var _last_index := -1.0
 
@@ -9,17 +9,24 @@ var _last_index := -1.0
 # Register hit
 func hit(time: float) -> bool:
 	var result := false
-	var atcharam_length := 60 / apm
-	
-	match time:
-		0.5, 0.75, 1.5, 1.75:
-			return pattern.size() > 0 and apm == 60 or apm == 80
+	var atcharam_length := 60.0 / apm
 	
 	# Find current index and check if it's close enough to integral index
-	var index := time / atcharam_length
-	var close_index = round(index)
+	var index := 0.0
+	match pattern:
+		[4]:
+			index = time / atcharam_length
+		[2, 2]:
+			index = time / atcharam_length * 2
+		[1, 1, 1, 1]:
+			index = time / atcharam_length * 4
+		[2, 1, 1]:
+			index = time / atcharam_length * 4
+			if int(index) % 4 == 1:
+				index -= 0.5
+			
+	var close_index := round(index)
 	var buffer := abs(index - close_index)
-	
 	if buffer < 0.15:
 		result = _last_index != close_index
 			
