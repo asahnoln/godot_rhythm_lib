18dedc139134b887e24462ee9606a76e001a9a15
Arthur Aslanyan <arthur.e.aslanyan@gmail.com>
2021-10-01 23:43:52 +0600

    Commiting sins, refactoring tests
    - [CHANGED] Tests for patterns refactored to table tests
    - [ADDED] Sins for [4] pattern

--- scripts/beat_processor.gd
+++ scripts/beat_processor.gd
@@ -4,20 +4,30 @@ var apm := 60.0
 var pattern := [4]
 
 var _last_index := -1.0
+const JATHI := 4
 
 
 # Register hit
 func hit(time: float) -> bool:
 	var result := false
-	var atcharam_length := 60.0 / apm
+	var matra_length := 60.0 / apm / JATHI
 	
-	# Find current index and check if it's close enough to integral index
-	var index := time / atcharam_length * 4
+	# Find current matra index and check if it's close enough to integral index
+	var index := time / matra_length # 4 is jathi (matras per atcharam)
 	match pattern:
 		[4]:
-			index /= 4
+			var matras := [1, 0, 0, 0]
+			var close_index := round(index)
+			var buffer := abs(index - close_index)
+			var res := false
+			if buffer < 0.5:
+				res = _last_index != close_index and matras[int(close_index) % 4] == 1
+					
+			_last_index = close_index
+			return res
 		[2, 2]:
-			index /= 2
+			if int(index) % 4 in [1, 3]:
+				index -= 0.5
 		[1, 1, 1, 1]:
 			pass
 		[2, 1, 1]:
@@ -31,7 +41,6 @@ func hit(time: float) -> bool:
 				index -= 0.5
 			
 	var close_index := round(index)
-	print("last ", _last_index, " current ", index)
 	var buffer := abs(index - close_index)
 	if buffer < 0.15:
 		result = _last_index != close_index
--- tests/beat.gd
+++ tests/beat.gd
@@ -20,7 +20,7 @@ func test_register_miss_if_extra_hit_within_buffer():
 	asserts.is_false(bp.hit(1), "Miss should be registered if extra hit done on the same time")
 	
 	_hit = bp.hit(9.477333)
-	asserts.is_false(bp.hit(9.5), "Miss should be registered if extra hit done on within same buffer")
+	asserts.is_false(bp.hit(9.5), "Miss should be registered if extra hit done on within the same buffer")
 
 
 func test_register_hit_on_beat():
@@ -30,6 +30,7 @@ func test_register_hit_on_beat():
 	
 
 func test_register_miss_out_of_beat():
+	print('miss out of beat')
 	asserts.is_false(bp.hit(0.5), "Miss should be registered out of beat")
 	
 
@@ -41,27 +42,76 @@ func test_different_tempo_hit_on_beat():
 
 func test_buffer_to_hit():
 	bp.apm = 120
+	print('buffer to hit')	
 	asserts.is_true(bp.hit(9.477333), "Hit should be registered with enough buffer until real beat")
 	asserts.is_true(bp.hit(47.557335), "Hit should be registered with enough buffer after real beat")
 	
 func test_beat_on_given_pattern():
 	bp.apm = 60
-	bp.pattern = [2, 2]
-	asserts.is_true(bp.hit(0), "Simple pattern: Hit should be registered on first pattern sol")
-	asserts.is_true(bp.hit(1.5), "Simple pattern: Hit should be registered on second pattern sol")
 	
-	bp.pattern = [2, 1, 1]
-	asserts.is_true(bp.hit(0.5), "Advanced: Hit should be registered on second pattern sol in first cycle")
-	asserts.is_true(bp.hit(0.75), "Advanced: Hit should be registered on third pattern sol in first cycle")
-	asserts.is_true(bp.hit(1.5), "Advanced: Hit should be registered on second pattern sol in second cycle")
-	asserts.is_true(bp.hit(1.75), "Advanced: Hit should be registered on third pattern sol in second cycle")
-	asserts.is_false(bp.hit(0.25), "Advanced: Miss should be registered on second pattern sol in first cycle")
+	var tests := {
+		[2, 2]: [
+			{
+				"desc": "Simple pattern: Hit should be registered on second pattern sol",
+				"time": 1.5,
+				"want": true,
+			},
+			{
+				"desc": "Simple pattern: Miss should be registered on first pattern sol",
+				"time": 0.25,
+				"want": false,
+			},
+		],
+		[2, 1, 1]: [
+			{
+				"desc": "Advanced: Hit should be registered on second pattern sol in first cycle",
+				"time": 0.5,
+				"want": true,
+			},
+			{
+				"desc": "Advanced: Hit should be registered on third pattern sol in second cycle",
+				"time": 1.75,
+				"want": true,
+			},
+			{
+				"desc": "Advanced: Miss should be registered on second pattern sol in first cycle",
+				"time": 0.25,
+				"want": false,
+			},
+		],
+		[1, 2, 1]: [
+			{
+				"desc": "Advanced 2: Hit should be registered on third pattern sol in second cycle",
+				"time": 1.75,
+				"want": true,
+			},
+			{
+				"desc": "Advanced 2: Miss should be registered on second pattern sol in first cycle",
+				"time": 0.5,
+				"want": false,
+			},
+		],
+		[1, 2, 1, 2, 1]: [
+			{
+				"desc": "Moving offbeat: Miss should be registered on second pattern sol in first cycle",
+				"time": 0.5,
+				"want": false,
+			},
+			{
+				"desc": "Moving offbeat: Miss should be registered on fourth pattern sol in first cycle",
+				"time": 1.25,
+				"want": false,
+			},
+			{
+				"desc": "Moving offbeat: Miss should be registered on second pattern sol in first cycle",
+				"time": 1.75,
+				"want": true,
+			},
+		],
+	}
 	
-	bp.pattern = [1, 2, 1]
-	asserts.is_false(bp.hit(0.5), "Advanced 2: Miss should be registered on second pattern sol in first cycle")
-	asserts.is_true(bp.hit(1.75), "Advanced 2: Hit should be registered on third pattern sol in second cycle")
-	
-	bp.pattern = [1, 2, 1, 2, 1]
-	asserts.is_false(bp.hit(0.5), "Moving offbeat: Miss should be registered on second pattern sol in first cycle")
-	asserts.is_false(bp.hit(1.25), "Moving offbeat: Miss should be registered on fourth pattern sol in first cycle")
-	asserts.is_true(bp.hit(1.75), "Moving offbeat: Miss should be registered on second pattern sol in first cycle")		
+	for pattern in tests:
+		bp.pattern = pattern
+		for tt in tests[pattern]:
+			asserts.is_equal(bp.hit(tt["time"]), tt["want"], tt["desc"])
+		
