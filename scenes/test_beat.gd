5b156d0e2abdc5a30831d164b43a824ad4d54ab8
Arthur Aslanyan <arthur.e.aslanyan@gmail.com>
2021-10-01 12:59:31 +0600

    Added scene for testing

---
+++ scenes/test_beat.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var bp := BeatProcessor.new()
+
+onready var _audio := $AudioStreamPlayer
+
+func _init() -> void:
+	bp.apm = 120
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		var _pos: float = _audio.get_playback_position()
+		print("position is ", _pos)
+		print("on beat is ", bp.hit(_pos))
